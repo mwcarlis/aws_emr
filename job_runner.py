863d910d59ae9e8518840f49726c81219d5853b0
Matthew W Carlis <matthew.carlis@sjsu.edu>
2015-08-31 15:17:08 -0700

    Initial commit jobs STARTING, but ending with errs.
    Now the cluster interface is starting up, but the jobs
    are terminating with errors for some reason.

--- job_runner.py
+++ job_runner.py
@@ -5,6 +5,7 @@
 # Open-Source/Free Imports
 from boto.s3.key import Key
 from boto.emr.step import StreamingStep
+from boto.emr.instance_group import InstanceGroup
 
 # Private-IP Imports Carlis/Koumis.
 from cluster_iface.connection import AwsConnection
@@ -63,6 +64,8 @@ class AwsJobRunner(AwsConnection, JobRunner):
         print self.steps
         print '\n'
         self.job_id = self.emr_conn.run_jobflow(name=name,
+                                                job_flow_role="EMR_EC2_DefaultRole",
+                                                service_role="EMR_DefaultRole",
                                                 log_uri=log_uri,
                                                 steps=self.steps)
 
@@ -71,16 +74,50 @@ class AwsJobRunner(AwsConnection, JobRunner):
         pass
 
 if __name__ == '__main__':
+    import time
     job_run = AwsJobRunner()
-    job_run.stream_steps('My wordcount example',
-                         's3n://facedata/wordSplitter.py',
-                         'aggregate',
-                         's3n://facedata/word_18',
-                         's3n://facedata/output')
 
-    job_run.init_job_flows('My Jobflow',
-                           's3n://facedata/logs/')
     print job_run.emr_conn
 
+    job_run.stream_steps('My wordcount example',
+                         's3n://elasticmapreduce/samples/wordcount/wordSplitter.py',
+                         'aggregate',
+                         's3n://elasticmapreduce/samples/wordcount/input',
+                         's3n://facedata/output')
 
+    job_run.init_job_flows('My Jobflow 3',
+                           's3n://facedata/logs')
+    while True:
+        print '\n\n\n'
+        for cl in job_run.emr_conn.list_clusters().clusters:
+            st = cl.status
+            print cl.name, cl.id, st.state
+        time.sleep(2)
+
+
+    # job_run.stream_steps('My wordcount example',
+    #                      's3n://facedata/wordSplitter.py',
+    #                      'aggregate',
+    #                      's3n://facedata/word_18',
+    #                      's3n://facedata/output')
+    # instance_groups = []
+    # instance_groups.append(InstanceGroup(
+    #     num_instances=1,
+    #     role="MASTER",
+    #     type="m1.small",
+    #     market="ON_DEMAND",
+    #     name="My cluster2"))
+    # instance_groups.append(InstanceGroup(
+    #     num_instances=2,
+    #     role="CORE",
+    #     type="m1.small",
+    #     market="ON_DEMAND",
+    #     name="Worker nodes"))
+    # instance_groups.append(InstanceGroup(
+    #     num_instances=2,
+    #     role="TASK",
+    #     type="m1.small",
+    #     market="SPOT",
+    #     name="My cheap spot nodes",
+    #     bidprice="0.002"))
 
