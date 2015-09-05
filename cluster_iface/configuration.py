ef1667769bc6c224a316e6351195adee3320cf15
Matthew W Carlis <matthew.carlis@sjsu.edu>
2015-09-05 12:20:52 -0700

    configuration.py: Now sets ~/.aws/<> config files.

--- cluster_iface/configuration.py
+++ cluster_iface/configuration.py
@@ -49,6 +49,34 @@ class AwsConfiguration(Configuration):
     def _write_config(self):
         """
         """
+        self._write_mrjob_config()
+        self._write_aws_config()
+
+    def _write_aws_config(self):
+        aws_path = os.path.join(self.AWS_SEC.HOME, '.aws')
+        if not os.path.isdir(aws_path):
+            os.mkdir(aws_path)
+
+        aws_conf = os.path.join(aws_path, 'config')
+        if not os.path.isfile(aws_conf):
+            conf = '[default]\nregion = us-east-1\n'
+            with open(aws_conf, 'w+') as file_d:
+                file_d.write(conf)
+
+        aws_cred = os.path.join(aws_path, 'credentials')
+        if not os.path.isfile(aws_cred):
+            cred = '[default]\n{} = {}\n{} = {}\n'
+            acc_kid = 'aws_access_key_id'
+            sec_kid = 'aws_secret_access_key'
+            data = cred.format(acc_kid,
+                               self.AWS_SEC.access_key_id,
+                               sec_kid,
+                               self.AWS_SEC.secret_access_key)
+            with open(aws_cred, 'w+') as file_d:
+                file_d.write(data)
+
+
+    def _write_mrjob_config(self):
         if not os.path.isfile(self.conf_file):
             # If it doesn't exist write it.
             with open(self.conf_file, 'w') as file_d:
